584d8050d09322a01e3a44e174086554c65d293c
saravanakumar-2003 <ssaravanakumar456@gmail.com>
2025-06-12 12:50:17 +0530

    Contact Entity created and connection established to postgres

---
+++ Identify-Reconcilience/src/main/java/com/example/Identify/Reconcilience/Entity/ContactEntity.java
@@ -0,0 +1,114 @@
+package com.example.Identify.Reconcilience.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ConditionalOnIssuerLocationJwtDecoder;
+
+@Entity(name = "contact")
+public class ContactEntity {
+
+    public ContactEntity(int id, String phoneNumber, String email, int linkedId, String linkPrecedence, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.linkedId = linkedId;
+        this.linkPrecedence = linkPrecedence;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public ContactEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    @Column
+    private int linkedId;
+
+    @Column
+    private String linkPrecedence;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLinkedId() {
+        return linkedId;
+    }
+
+    public void setLinkedId(int linkedId) {
+        this.linkedId = linkedId;
+    }
+
+    public String getLinkPrecedence() {
+        return linkPrecedence;
+    }
+
+    public void setLinkPrecedence(String linkPrecedence) {
+        this.linkPrecedence = linkPrecedence;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
---
+++ Identify-Reconcilience/src/main/java/com/example/Identify/Reconcilience/IdentifyReconcilienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Identify.Reconcilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentifyReconcilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentifyReconcilienceApplication.class, args);
+	}
+
+}
